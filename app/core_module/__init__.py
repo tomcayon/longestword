a30c92a64989fcb2f20d2f5b5e70f7e11cd4b01d
fime-space <72309049+fime-space@users.noreply.github.com>
2022-03-31 09:21:58 +0200

    Created Modules

---
+++ app/core_module/__init__.py
@@ -0,0 +1,12 @@
+"""
+Core module
+"""
+
+class AppCore():
+
+    def __init__(self,word_module,graphic_module):
+        self.word_module = word_module
+        self.graphic_engine = graphic_module
+
+    def launch_app(self):
+        pass
