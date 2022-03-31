0ef67e0288d595457fc9d031fbd498faffa41e44
fime-space <72309049+fime-space@users.noreply.github.com>
2022-03-31 09:36:14 +0200

    started writing graohic module

--- app/graphic_module/__init__.py
+++ app/graphic_module/__init__.py
@@ -1,3 +1,13 @@
 """
 Graphic Module
 """
+
+import tkinter as tk
+
+class AppWindow():
+    def __init__(self):
+        self.init_window()
+
+    def init_window(self):
+        self.window = tk.Tk()
+        self.window.title("Longest Word")
