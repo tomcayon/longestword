68c8b1bd4d699de0ea5753a6099ddcf3cd81d9a0
fime-space <72309049+fime-space@users.noreply.github.com>
2022-04-08 12:02:46 +0200

    Add files via upload

--- app/graphic_module/__init__.py
+++ app/graphic_module/__init__.py
@@ -1,16 +1,3 @@
 """
-Graphic Module
+Word Module
 """
-
-import pygame
-
-DIM = W,H = 1024,600
-
-class AppWindow():
-    def __init__(self):
-        pygame.init()
-        self.screen = pygame.display.set_mode(DIM)
-
-
-if __name__ == "__main__":
-    test = AppWindow()
